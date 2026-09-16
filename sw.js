4f73821921d418e18bcb23aaefe62a4b3a0d89f5
gregorbuerger <gregor.buerger@googlemail.com>
2026-09-16 15:42:32 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='wetter-v0.8';
-const SHELL=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+const CACHE='wetter-v0.9';
+const SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)));});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]));});
 self.addEventListener('fetch',e=>{
